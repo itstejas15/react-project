4e39f5d40c974209ce00058ae22ee1f99661d35b
Tejas Patil <tejas_patil2@persistent.com>
2023-05-17 20:55:03 +0530

    added routing

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+	return (
+		<div style={{ margin: "8px", display: "flex", alignItems: "center" }}>
+			<h3>Contact&nbsp;:</h3> &nbsp;9404475442
+		</div>
+	)
+}
+
+export default Contact
---
+++ src/components/NoPage.jsx
@@ -0,0 +1,9 @@
+const NoPage= () => {
+    return(
+        <div style={{display:'flex' , justifyContent:'center' , alignItems:'center', height:'100vh'}}>
+            <h3>Page doesn&apos;t exist</h3>
+        </div>
+    )
+}
+
+export default NoPage;
